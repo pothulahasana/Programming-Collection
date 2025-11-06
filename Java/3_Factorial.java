981ceeade88e7b04b039a3d3734c160c754b6136
pothulahasana <hasanap2007@gmail.com>
2025-11-06 16:54:42 +0530

    Create 3_Factorial.java

---
+++ Java/3_Factorial.java
@@ -0,0 +1,8 @@
+public class Factorial {
+    static int factorial(int n) {
+        return n <= 1 ? 1 : n * factorial(n - 1);
+    }
+    public static void main(String[] args) {
+        System.out.println("Factorial of 5: " + factorial(5));
+    }
+}
