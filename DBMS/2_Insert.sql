0cfaf1445a257029b9c22532b62d88bbbfef59da
pothulahasana <hasanap2007@gmail.com>
2025-11-06 16:58:45 +0530

    Create 2_Insert.sql

---
+++ DBMS/2_Insert.sql
@@ -0,0 +1,5 @@
+-- Insert Data
+INSERT INTO Students (id, name, age, email) VALUES
+(1, 'Alice', 20, 'alice@example.com'),
+(2, 'Bob', 21, 'bob@example.com'),
+(3, 'Charlie', 22, 'charlie@example.com');
